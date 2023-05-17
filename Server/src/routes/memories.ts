48d091af104dc32d58947188dd51ecbc1f89a4eb
Iarlen <iarlensr@hotmail.com>
2023-05-17 20:31:48 -0300

    feat: Finishing Lesson 2.

---
+++ Server/src/routes/memories.ts
@@ -0,0 +1,101 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+import { z } from 'zod'
+
+export const memoriesRoutes = async (app: FastifyInstance) => {
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      }
+    })
+  })
+
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
+  })
+
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: 'a03c3b65-38c2-45fb-95fd-ae12b6420fba',
+      },
+    })
+
+    return memory
+  })
+
+  app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+
+    return memory
+  })
+
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
+  })
+}
--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,46 +1,65 @@
 import { StatusBar } from 'expo-status-bar'
-import { useState } from 'react'
-import { Text, View, Button, TextInput } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import BlurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
-  const [item, setItem] = useState('')
-  const [listItem, setListItem] = useState<string[]>([])
+  const [hasLoaderFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
 
-  const addItem = (item: string) => {
-    setListItem((oldList) => [...oldList, item])
-    setItem('')
+  if (!hasLoaderFonts) {
+    return null
   }
 
-  const removeItem = (item: string) => {
-    setListItem(listItem.filter((itemList) => itemList !== item))
-  }
   return (
-    <View className="box-border flex-1 bg-gray-700 p-2">
-      <View className="gap-3 pt-12">
-        <Text className="text-xl capitalize text-white">Lista de Tarefas</Text>
-        <View className="flex-row justify-between gap-1">
-          <TextInput
-            className="w-3/4 rounded bg-gray-400 p-2 font-semibold italic text-white"
-            placeholder="Digite um item"
-            value={item}
-            onChangeText={(text) => setItem(text)}
-          />
-          <Button title="ADD ITEM" onPress={() => addItem(item)} />
+    <ImageBackground
+      source={BlurBg}
+      className="relative flex-1 bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes de sua jornada e compatilhe (se quiser)
+            com o mundo!
+          </Text>
         </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar Lembrança
+          </Text>
+        </TouchableOpacity>
       </View>
-      <View className="w-full flex-col gap-2 pt-2">
-        {listItem &&
-          listItem.map((item) => (
-            <View
-              key={item}
-              className="flex w-full flex-row items-center justify-between"
-            >
-              <Text className="italic text-white">- {item}</Text>
-              <Button title="remover" onPress={() => removeItem(item)} />
-            </View>
-          ))}
-      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
       <StatusBar style="light" />
-    </View>
+    </ImageBackground>
   )
 }
